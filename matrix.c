716007294bed634f91575e598ea18402400598a1
roshanmadhushanka <alwisroshan.13@cse.mrt.ac.lk>
2017-06-17 19:28:46 +0530

    Update the optimizedParallel computing logic

--- matrix.c
+++ matrix.c
@@ -10,52 +10,87 @@ https://computing.llnl.gov/tutorials/openMP/samples/C/omp_mm.c
 
 typedef double TYPE;
 
-void parallelMultiply(TYPE** matrixA, TYPE** matrixB, TYPE** result, int dimension);
 void sequentialMultiply(TYPE** matrixA, TYPE** matrixB, TYPE** result, int dimension);
+void parallelMultiply(TYPE** matrixA, TYPE** matrixB, TYPE** result, int dimension);
+void optimizedParallelMultiply(TYPE** matrixA, TYPE** matrixB, TYPE** result, int dimension);
 TYPE** randomMatrix(int dimension);
 TYPE** zeroMatrix(int dimension);
+TYPE* zeroFlatMatrix(int dimension);
+TYPE* flatMatrix(TYPE** matrix, int dimension);
 void displayMatrix(TYPE** matrix, int dimension);
+void displayFlatMatrix(TYPE* matrix, int dimension);
 
 int main(int argc, char* argv[]){
 	int dimension = strtol(argv[1], NULL, 10);
 	TYPE** matrixA = randomMatrix(dimension);
 	TYPE** matrixB = randomMatrix(dimension);
-	TYPE** resultMatrix = zeroMatrix(dimension);
-	parallelMultiply(matrixA, matrixB, resultMatrix, dimension);
-	sequentialMultiply(matrixA, matrixB, resultMatrix, dimension);
+	TYPE** matrixResult = zeroMatrix(dimension);
+	
+
+	
+	optimizedParallelMultiply(matrixA, matrixB, matrixResult, dimension);
+	parallelMultiply(matrixA, matrixB, matrixResult, dimension);
 
 
-	// displayMatrix(matrixA, dimension);
-	// displayMatrix(matrixB, dimension);
-	// displayMatrix(resultMatrix, dimension);
+	free(matrixA);
+	free(matrixB);
+	free(matrixResult);
 	return 0;
+}
+
+void sequentialMultiply(TYPE** matrixA, TYPE** matrixB, TYPE** result, int dimension){
+	clock_t start, end;
+	double cpu_time_used;
+	int i, j, k;
+
+	start = clock();
+
+	/* Begin process */
+
+    for (i=0; i<dimension; i++)
+    {
+        for (j=0; j<dimension; j++)
+        {
+            for (k=0; k<dimension; k++){
+                result[i][j] += matrixA[i][k] * matrixB[k][j] ;
+            }
+        }
+    }
+    /* End process */
+
+    end = clock();
+
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("Result for Sequential Matrix Multiplication\n");
+	printf("-------------------------------------------\n");
+	printf("Dimension  : %d\n", dimension);
+	printf("Time taken : %f seconds\n", cpu_time_used);
 }
 
 void parallelMultiply(TYPE** matrixA, TYPE** matrixB, TYPE** result, int dimension){
 	clock_t start, end;
 	double cpu_time_used;
+	int i, j, k, n_thread;
 
 	start = clock();
 
 	/* Begin process */
-	int i, j, k, n_thread;
+
 	#pragma omp parallel shared(matrixA, matrixB, result) private(i, j, k)
 	{ 
 		n_thread = omp_get_num_threads();
 		#pragma omp for schedule(static)
 	    for (i=0; i<dimension; i++)
 	    {
-	    	int tot;
 	        for (j=0; j<dimension; j++)
 	        {	
-	        	tot = 0;
 	            for (k=0; k<dimension; k++){
-	                tot += matrixA[i][k] * matrixB[k][j];
+	                result[i][j] += matrixA[i][k] * matrixB[k][j];
 	            }
-	            result[i][j] = tot;
 	        }
 	    }
 	}
+
     /* End process */
 
     end = clock();
@@ -65,37 +100,54 @@ void parallelMultiply(TYPE** matrixA, TYPE** matrixB, TYPE** result, int dimensi
 	printf("-------------------------------------------\n");
 	printf("Dimension  : %d\n", dimension);
 	printf("Time taken : %f seconds\n", cpu_time_used);
+	printf("--------------------------------------------\n\n");
 }
 
-void sequentialMultiply(TYPE** matrixA, TYPE** matrixB, TYPE** result, int dimension){
+void optimizedParallelMultiply(TYPE** matrixA, TYPE** matrixB, TYPE** result, int dimension){
 	clock_t start, end;
 	double cpu_time_used;
+	int i, j, k, tot, n_thread;
 
 	start = clock();
 
-	/* Begin process */
-	int i, j, k, tot;
+	/* Begining of process */
 
-    for (i=0; i<dimension; i++)
-    {
-        for (j=0; j<dimension; j++)
-        {
-        	tot = 0;
-            for (k=0; k<dimension; k++){
-                tot += matrixA[i][k] * matrixB[k][j] ;
-            }
-            result[i][j] = tot;
-        }
-    }
-    /* End process */
+	TYPE* matrixFlatA = flatMatrix(matrixA, dimension);
+	TYPE* matrixFlatB = flatMatrix(matrixB, dimension);
 
-    end = clock();
-	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-	printf("Result for Sequential Matrix Multiplication\n");
-	printf("-------------------------------------------\n");
+	#pragma omp parallel shared(matrixFlatA, matrixFlatB, result) private(i, j, k, tot)
+	{
+		n_thread = omp_get_num_threads();
+		#pragma omp for schedule(static)
+		for(i=0; i<dimension; i++){
+			for(j=0; j<dimension; j++){
+				tot = 0;
+				for(k=0; k<dimension; k++){
+					tot += matrixFlatA[dimension * i + k] * matrixFlatB[dimension * k + j];
+				}
+				result[i][j] = tot;
+			}
+		}
+	}
+
+	free(matrixFlatA);
+	free(matrixFlatB);
+
+	/* End of process*/
+
+	end = clock();
+
+
+
+	cpu_time_used = ((double) (end - start)) / (CLOCKS_PER_SEC * n_thread);
+	printf("Result for : Parallel Flat Matrix multiplication\n");
+	printf("------------------------------------------------\n");
 	printf("Dimension  : %d\n", dimension);
 	printf("Time taken : %f seconds\n", cpu_time_used);
+	printf("------------------------------------------------\n\n");
+
 }
+
 
 TYPE** randomMatrix(int dimension){
 	TYPE** matrix = malloc(dimension * sizeof(TYPE*));
@@ -103,10 +155,12 @@ TYPE** randomMatrix(int dimension){
 		matrix[i] = malloc(dimension * sizeof(TYPE));
 	}
 
+	//Random seed
 	srandom(time(0)+clock()+random());
+
 	for(int i=0; i<dimension; i++){
 		for(int j=0; j<dimension; j++){
-			matrix[i][j] = rand() % 100;
+			matrix[i][j] = rand() % 1000 + 1;
 		}
 	}
 	return matrix;
@@ -127,10 +181,39 @@ TYPE** zeroMatrix(int dimension){
 	return matrix;
 }
 
+TYPE* zeroFlatMatrix(int dimension){
+	TYPE* matrix = malloc(dimension * dimension * sizeof(TYPE));
+
+	srandom(time(0)+clock()+random());
+	for(int i=0; i<dimension*dimension; i++){
+		matrix[i] = 0;
+	}
+	return matrix;
+}
+
+TYPE* flatMatrix(TYPE** matrix, int dimension){
+	TYPE* flatMatrix = malloc(dimension * dimension * sizeof(TYPE));
+	for(int i=0; i<dimension; i++){
+		for(int j=0; j<dimension; j++){
+			flatMatrix[i * dimension + j] = matrix[i][j];
+		}
+	}
+	return flatMatrix;
+}
+
 void displayMatrix(TYPE** matrix, int dimension){
 	for(int i=0; i<dimension; i++){
 		for(int j=0; j<dimension; j++){
 			printf("%f\t", matrix[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+void displayFlatMatrix(TYPE* matrix, int dimension){
+	for(int i=0; i<dimension; i++){
+		for(int j=0; j<dimension; j++){
+			printf("%f\t", matrix[dimension * i + j]);
 		}
 		printf("\n");
 	}
